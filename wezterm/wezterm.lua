3928a763c84bf1a9a7cd75aa64aa18447ba01619
Syd Amir <hemedani@gmail.com>
2022-01-21 12:27:32 +0330

    add tab relative navigation for wez

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,4 +1,5 @@
 local wezterm = require("wezterm")
+
 return {
 	font = wezterm.font_with_fallback({
 		"Cascadia Code Light",
@@ -60,6 +61,105 @@ return {
 		},
 	},
 
+	window_frame = {
+		-- The font used in the tab bar.
+		-- Roboto Bold is the default; this font is bundled
+		-- with wezterm.
+		-- Whatever font is selected here, it will have the
+		-- main font setting appended to it to pick up any
+		-- fallback fonts you may have used there.
+		font = wezterm.font({ family = "Cascadia Code Semibold" }),
+
+		-- The size of the font in the tab bar.
+		-- Default to 10. on Windows but 12.0 on other systems
+		font_size = 13.0,
+
+		-- The overall background color of the tab bar when
+		-- the window is focused
+		active_titlebar_bg = "#333333",
+
+		-- The overall background color of the tab bar when
+		-- the window is not focused
+		inactive_titlebar_bg = "#333333",
+
+		-- The color of the inactive tab bar edge/divider
+		inactive_tab_edge = "#575757",
+	},
+
+	-- colors = {
+	-- 	tab_bar = {
+	-- 		-- The color of the strip that goes along the top of the window
+	-- 		-- (does not apply when fancy tab bar is in use)
+	-- 		background = "#0b0022",
+	--
+	-- 		-- The active tab is the one that has focus in the window
+	-- 		active_tab = {
+	-- 			-- The color of the background area for the tab
+	-- 			bg_color = "#2b2042",
+	-- 			-- The color of the text for the tab
+	-- 			fg_color = "#c0c0c0",
+	--
+	-- 			-- Specify whether you want "Half", "Normal" or "Bold" intensity for the
+	-- 			-- label shown for this tab.
+	-- 			-- The default is "Normal"
+	-- 			intensity = "Normal",
+	--
+	-- 			-- Specify whether you want "None", "Single" or "Double" underline for
+	-- 			-- label shown for this tab.
+	-- 			-- The default is "None"
+	-- 			underline = "None",
+	--
+	-- 			-- Specify whether you want the text to be italic (true) or not (false)
+	-- 			-- for this tab.  The default is false.
+	-- 			italic = false,
+	--
+	-- 			-- Specify whether you want the text to be rendered with strikethrough (true)
+	-- 			-- or not for this tab.  The default is false.
+	-- 			strikethrough = false,
+	-- 		},
+	--
+	-- 		-- Inactive tabs are the tabs that do not have focus
+	-- 		inactive_tab = {
+	-- 			bg_color = "#1b1032",
+	-- 			fg_color = "#808080",
+	--
+	-- 			-- The same options that were listed under the `active_tab` section above
+	-- 			-- can also be used for `inactive_tab`.
+	-- 		},
+	--
+	-- 		-- You can configure some alternate styling when the mouse pointer
+	-- 		-- moves over inactive tabs
+	-- 		inactive_tab_hover = {
+	-- 			bg_color = "#3b3052",
+	-- 			fg_color = "#909090",
+	-- 			italic = true,
+	--
+	-- 			-- The same options that were listed under the `active_tab` section above
+	-- 			-- can also be used for `inactive_tab_hover`.
+	-- 		},
+	--
+	-- 		-- The new tab button that let you create new tabs
+	-- 		new_tab = {
+	-- 			bg_color = "#1b1032",
+	-- 			fg_color = "#808080",
+	--
+	-- 			-- The same options that were listed under the `active_tab` section above
+	-- 			-- can also be used for `new_tab`.
+	-- 		},
+	--
+	-- 		-- You can configure some alternate styling when the mouse pointer
+	-- 		-- moves over the new tab button
+	-- 		new_tab_hover = {
+	-- 			bg_color = "#3b3052",
+	-- 			fg_color = "#909090",
+	-- 			italic = true,
+	--
+	-- 			-- The same options that were listed under the `active_tab` section above
+	-- 			-- can also be used for `new_tab_hover`.
+	-- 		},
+	-- 	},
+	-- },
+
 	native_macos_fullscreen_mode = true,
 
 	initial_cols = 150,
@@ -82,11 +182,18 @@ return {
 		{ key = "l", mods = "CMD|CTRL", action = wezterm.action({ ActivatePaneDirection = "Right" }) },
 		{ key = "k", mods = "CMD|CTRL", action = wezterm.action({ ActivatePaneDirection = "Up" }) },
 		{ key = "j", mods = "CMD|CTRL", action = wezterm.action({ ActivatePaneDirection = "Down" }) },
+		-- Pane Cycles
+		{ key = "n", mods = "CMD|CTRL", action = wezterm.action({ ActivatePaneDirection = "Next" }) },
+		{ key = "p", mods = "CMD|CTRL", action = wezterm.action({ ActivatePaneDirection = "Prev" }) },
 		-- Pane Resize
 		{ key = "H", mods = "CMD|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Left", 2 } }) },
 		{ key = "J", mods = "CMD|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Down", 2 } }) },
 		{ key = "K", mods = "CMD|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Up", 2 } }) },
 		{ key = "L", mods = "CMD|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Right", 2 } }) },
+
+		-- TAB section
+		{ key = ",", mods = "CMD", action = wezterm.action({ ActivateTabRelativeNoWrap = 1 }) },
+		{ key = "m", mods = "CMD", action = wezterm.action({ ActivateTabRelativeNoWrap = -1 }) },
 
 		-- search for the string "hash" matching regardless of case
 		{ key = "F", mods = "CMD|SHIFT", action = wezterm.action({ Search = { CaseInSensitiveString = "hash" } }) },
